374c27aaf00d314576018d186688cfa30ce841a6
Alex <49156159+sanyaaa777@users.noreply.github.com>
2025-05-11 15:21:01 +0200

    Initial commit

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({avatar, name, isOnline}) => {
+    return (
+        <div className={styles.card}>
+            <img src={avatar} alt="Avatar" width="48" />
+            <p className={styles.name}>{name}</p>
+            <p className={isOnline ? styles.online : styles.offline}>
+                {isOnline ? 'Online' : 'Offline'}
+            </p>
+        </div>
+    ) 
+    
+}
+
+export default FriendListItem;
